c4f7579616067f1c977af8970a1f72fec4cb0397
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-21 14:51:03 +0700

    Setup UI

--- app/src/main/java/com/rujirakongsomran/jc_rabbitapp/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_rabbitapp/MainActivity.kt
@@ -3,36 +3,68 @@ package com.rujirakongsomran.jc_rabbitapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
 import com.rujirakongsomran.jc_rabbitapp.ui.theme.JC_RabbitAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JC_RabbitAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+
+                ) {
+                    val viewModel: MainViewModel = hiltViewModel()
+                    val rabbit = viewModel.state.value.rabbit
+                    val isLoading = viewModel.state.value.isLoading
+                    rabbit?.let {
+                        Image(
+                            painter = rememberImagePainter(
+                                data = rabbit.imageUrl,
+                                builder = {
+                                    crossfade(true)
+                                }
+                            ), contentDescription = " Rabbit"
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = rabbit.name,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(text = rabbit.description)
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                    Button(
+                        onClick = viewModel::getRandomRabbit,
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text(text = "Next rabbit!")
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    if(isLoading){
+                        CircularProgressIndicator()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JC_RabbitAppTheme {
-        Greeting("Android")
     }
 }
