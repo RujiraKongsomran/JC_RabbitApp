bf5b57b98375d3fcdafb7df389cac542a628b43f
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-11-21 14:12:29 +0700

    AppModule

--- app/src/main/java/com/rujirakongsomran/jc_rabbitapp/data/RabbitsApi.kt
+++ app/src/main/java/com/rujirakongsomran/jc_rabbitapp/data/RabbitsApi.kt
@@ -5,4 +5,8 @@ import retrofit2.http.GET
 interface RabbitsApi {
     @GET("/randomrabbit")
     suspend fun getRandomRabbit(): Rabbit
+
+    companion object {
+        const val BASE_URL = "http://192.168.1.6:8100"
+    }
 }
